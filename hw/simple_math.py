f7c07e6b172d977f93faf73dfb5665034b7f3217
AndreyLapko1 <andreycgo765@gmail.com>
2025-01-29 16:07:01 +0300

    update

--- hw/simple_math.py
+++ hw/simple_math.py
@@ -5,3 +5,16 @@ class SimpleMath:
 
     def cube(self, num):
         return num ** 3
+
+
+
+a = [1,2,3,4,5]
+b = iter(a)
+c = iter(a)
+c.__next__()
+lg = 0
+for num1, num2 in zip(b, c):
+    print(num1, num2)
+    lg += num2 -num1
+
+print(lg)
--- hw/test_simple_math.py
+++ hw/test_simple_math.py
@@ -1,16 +1,29 @@
 import pytest
 from simple_math import SimpleMath
 
+
 @pytest.fixture
 def math():
     return SimpleMath()
 
-def test_square(math):
-    actual_result = math.square(2)
-    expected_result = 4
+@pytest.mark.parametrize('a, b', [
+    (2,4),
+    (-3,-27),
+
+])
+
+
+
+# @pytest.mark.skipif(1 == 1,reason="not implemented")
+def test_square(math, a, b):
+
+    actual_result = math.square(a)
+    expected_result = b
     assert actual_result == expected_result
 
-def test_cube(math):
-    actual_result = math.cube(-3)
-    expected_result = -27
+def test_cube(math,a,b):
+    actual_result = math.cube(a)
+    expected_result = b
     assert actual_result == expected_result
+
+
